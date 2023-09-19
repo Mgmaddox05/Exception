ce2f57dc69d516b69f6e5cb507931b8c2565c74c
Mgmaddox05 <125920278+Mgmaddox05@users.noreply.github.com>
2023-09-19 15:35:00 -0500

    Update

--- src/App.java
+++ src/App.java
@@ -3,7 +3,40 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        IntegerException intExe = new IntegerException(scan);
+        
+        
+        try {
+            System.out.print("Enter an integer: ");
+            String integer = scan.nextLine();
+            if (!integer.equals("exit"))
+            {
+                 int integerTru = Integer.parseInt(integer);
+                 factor(integerTru);
+            }
+            
+    
+        }
+        catch (InputMismatchException e)
+        {
+            System.out.println( "Invaild Input. Please enter an integer Goofy!");
+        }
+
+
+    }
+
+    public static void factor(int n) {
+        
+        
+        
+        for (int i = 1; i <= n; i++)
+         {
+            if(n % i == 0)
+            {
+                System.out.println("A factor of " + n + " is: " + i);
+            }
+            
+        }
+       
         
     }
 }
